4db18a87022d5103c9956c773326fb135448228b
Sami Hakani <sami.hakani@gmail.com>
2023-08-10 11:29:41 -0700

    Passed 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (inputWord) {
+    inputWord = inputWord.toLowerCase().replace(/\W/g, ''); // strip to alphanumeric
+    let l = inputWord.length - 1;
+    for (let i = 0; i < l; i++) {
+        if (inputWord[i] != inputWord[l-i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
