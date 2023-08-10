f4229f6740a37d393b0ae0b92667627248580c96
Sami Hakani <sami.hakani@gmail.com>
2023-08-10 10:53:34 -0700

    Solved problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    myArray = arguments[0];
+    for (let i = 1; i < arguments.length; i++) { // loop through args
+        argumentNow = arguments[i];
+        argumentIndex = myArray.indexOf(argumentNow);
+        if (argumentIndex > -1) {
+            myArray.splice(argumentIndex, 1); // delete an element at the index if possible
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
