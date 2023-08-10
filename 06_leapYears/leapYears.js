5b3f991e1d4439fa292d141fa97b7b76efc7f440
Sami Hakani <sami.hakani@gmail.com>
2023-08-10 11:07:59 -0700

    Solved problem 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // leap years are divisible by 4
+    // leap years are not divisible by 100
+    // leap years are divisible by 400
+    if(year%4 == 0) {
+        if(year%100 == 0) {
+            if(year%400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
