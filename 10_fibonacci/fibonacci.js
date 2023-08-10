03135cb09836a7249311b2c54f243d5315c93c0f
Sami Hakani <sami.hakani@gmail.com>
2023-08-10 11:33:29 -0700

    Dynamically finding Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 2) {
+        return 1;
+    }
+    else {
+        let a = 1; let b = 1; // fibonacci seeds
+        let temp = 0; // temporary container
+        while (n > 2) {
+            // a, b = b, a+b
+            temp = a;
+            a = b;
+            b += temp;
+            n -= 1;
+        }
+        return b;
+    }
 };
 
 // Do not edit below this line
