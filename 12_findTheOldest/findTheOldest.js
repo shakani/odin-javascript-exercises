5707d4822469e5c27da9583ccf740f56e71d9caf
Sami Hakani <sami.hakani@gmail.com>
2023-08-10 12:18:07 -0700

    Solved 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson = null;
 
+    for (let i = 0; i < people.length; i++) { // loop through people
+        let person = people[i]; 
+        personProperties = Object.keys(person);
+
+        if(personProperties.length < 3) { // person is alive
+            // set death date to NOW
+            let now = new Date()
+            let yearNow = now.getFullYear()
+            person["yearOfDeath"] = yearNow;
+        }
+            
+        let personAge = person["yearOfDeath"] - person["yearOfBirth"];
+        
+        // check if this person is older
+        if (personAge > oldestAge) {
+            oldestAge = personAge;
+            oldestPerson = person;
+        }
+    }
+    
+    return oldestPerson;
 };
 
 // Do not edit below this line
