2cafdfff3c4a5597728a377a4be2da347719ae85
Sami Hakani <sami.hakani@gmail.com>
2023-08-10 11:39:44 -0700

    Passed 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for (let i = 0; i < books.length; i++) {
+        book = books[i];
+        titles.push(book["title"]);
+    }
+    return titles;
 };
 
 // Do not edit below this line
