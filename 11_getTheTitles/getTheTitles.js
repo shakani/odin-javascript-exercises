09423b48f642613f55b93e8b86f8bd94caa0be45
Sami Hakani <sami.hakani@gmail.com>
2023-08-10 18:13:19 -0700

    Solved 11 with map

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,10 +1,11 @@
 const getTheTitles = function(books) {
-    let titles = [];
-    for (let i = 0; i < books.length; i++) {
-        book = books[i];
-        titles.push(book["title"]);
-    }
-    return titles;
+    // let titles = [];
+    // for (let i = 0; i < books.length; i++) {
+    //     book = books[i];
+    //     titles.push(book["title"]);
+    // }
+    // return titles;
+    return books.map((book) => book.title);
 };
 
 // Do not edit below this line
