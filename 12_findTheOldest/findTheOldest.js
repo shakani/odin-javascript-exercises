e476e87a41df70ed8346e052d2cb7c581d9f587f
Sami Hakani <sami.hakani@gmail.com>
2023-08-10 18:29:38 -0700

    Solved 12 with array methods

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,28 +1,17 @@
 const findTheOldest = function(people) {
-    let oldestAge = 0;
-    let oldestPerson = null;
-
-    for (let i = 0; i < people.length; i++) { // loop through people
-        let person = people[i]; 
-        personProperties = Object.keys(person);
-
-        if(personProperties.length < 3) { // person is alive
-            // set death date to NOW
-            let now = new Date()
-            let yearNow = now.getFullYear()
-            person["yearOfDeath"] = yearNow;
+    for (let i = 0; i < people.length; i++) {
+        if (!people[i].yearOfDeath) {
+            people[i].yearOfDeath = (new Date()).getFullYear();
         }
-            
-        let personAge = person["yearOfDeath"] - person["yearOfBirth"];
-        
-        // check if this person is older
-        if (personAge > oldestAge) {
-            oldestAge = personAge;
-            oldestPerson = person;
-        }
-    }
-    
-    return oldestPerson;
+    } // put passing year to now 
+
+    people = people.sort((a, b) => {
+        aAge = a.yearOfDeath - a.yearOfBirth;
+        bAge = b.yearOfDeath - b.yearOfBirth;
+        return aAge > bAge ? -1 : 1;
+    });
+    console.log(people);
+    return people[0];
 };
 
 // Do not edit below this line
